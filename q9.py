dc2a86d546fafb94d8ab7c3df408ca2a16dd4fde
bartuuyar <88313911+bartuuyar@users.noreply.github.com>
2024-05-14 18:27:25 +0300

    Update q9.py

--- q9.py
+++ q9.py
@@ -1,11 +1,12 @@
 import re
 
 def extract_url_info(url):
+    #regular expression to capture pattern
     pattern = r'<[a-z]+>https?://([a-z0-9._]+)</[a-z]+>'
     
     match = re.match(pattern, url, flags=re.IGNORECASE)
     return match.group(1)
 
-url = "<SSL>https://xcd32112.smart_meter.com</SSL>"
+url = "<url>https://xcd32112.smart_meter.com</url>"
 output = extract_url_info(url)
 print(output)
